b5d8a1b79d632142dcb3b8282fb2e9374d643de9
Ilia O. <Ilia O.>
2024-09-04 23:11:02 -0700

    Odometry fix

--- kaiaai-esp32/kaiaai-esp32.ino
+++ kaiaai-esp32/kaiaai-esp32.ino
@@ -322,9 +322,18 @@ void publishTelem(unsigned long step_time_us) {
     joint_prev_pos[i] = joint[i].pos;
   }
 
-  calcOdometry2(step_time_us, joint_pos_delta[0], joint_pos_delta[1]);
+  calcOdometry(step_time_us, joint_pos_delta[0], joint_pos_delta[1]);
+//  calcOdometry2(step_time_us, joint_pos_delta[0], joint_pos_delta[1]);
 
   RCSOFTCHECK(rcl_publish(&telem_pub, &telem_msg, NULL));
+
+  
+  Serial.print(telem_msg.odom_pos_x, 8);
+  Serial.print("\t");
+  Serial.print(telem_msg.odom_pos_y, 8);
+  Serial.print("\t");
+  Serial.println(telem_msg.odom_pos_yaw, 8);
+  
   telem_msg.lds.size = 0;
   telem_msg.seq++;
 }
@@ -340,7 +349,12 @@ void calcOdometry(unsigned long step_time_us, float joint_pos_delta_right,
 
   // TODO use Runge-Kutta integration for better accuracy
   float average_distance = (distance_right + distance_left) * 0.5;
-  float d_yaw = asin((distance_left - distance_right)*cfg.wheel_track_recip);
+  float d_yaw = (distance_left - distance_right)*cfg.wheel_track_recip;
+  if (abs(d_yaw) > 1) {
+    printNB("WARNING: odometry asin() domain out of bounds. Check motor encoders.");
+    d_yaw = d_yaw > 0 ? 1 : -1;
+  }
+  d_yaw = asin(d_yaw);
 
   // Average angle during the motion
   float average_angle = d_yaw*0.5 + telem_msg.odom_pos_yaw;
@@ -362,75 +376,6 @@ void calcOdometry(unsigned long step_time_us, float joint_pos_delta_right,
     telem_msg.odom_pos_yaw -= TWO_PI;
   else if (telem_msg.odom_pos_yaw < -PI)
     telem_msg.odom_pos_yaw += TWO_PI;
-
-  float d_time = 1e-6 * (float)step_time_us;
-  telem_msg.odom_vel_x = average_distance / d_time;
-  telem_msg.odom_vel_yaw = d_yaw / d_time;
-}
-
-void calcOdometry2(unsigned long step_time_us, float joint_pos_delta_right,
-  float joint_pos_delta_left) {
-
-  if (step_time_us == 0)
-    return;
-
-  float distance_right = -joint_pos_delta_right * cfg.wheel_radius;
-  float distance_left = joint_pos_delta_left * cfg.wheel_radius;
-/*
-  Serial.print(step_time_us);
-  Serial.print('\t');
-  Serial.print(distance_right);
-  Serial.print('\t');
-  Serial.print(distance_left);
-  Serial.print('\t');
-
-  Serial.print(motorLeft.getEncoderValue());
-  Serial.print('\t');
-  Serial.println(motorRight.getEncoderValue());
-*/
-//  Serial.print('\t');
-//  Serial.print(motorLeft.getCurrentRPM());
-//  Serial.print('\t');
-//  Serial.print(motorRight.getCurrentRPM());
-//  Serial.print('\t');
-//  Serial.print(motorLeft.getCurrentPWM());
-//  Serial.print('\t');
-//  Serial.print(motorRight.getCurrentPWM());
-//  Serial.print('\t');
-//  Serial.print(motorLeft.getTargetRPM());
-//  Serial.print('\t');
-//  Serial.println(motorRight.getTargetRPM());
-
-  // TODO use Runge-Kutta integration for better accuracy
-  float average_distance = (distance_right + distance_left) * 0.5;
-  float d_yaw = (distance_left - distance_right)*cfg.wheel_track_recip;
-
-//  Serial.print(cfg.wheel_track_recip);
-//  Serial.print('\t');
-//  Serial.print(average_distance);
-//  Serial.print('\t');
-//  Serial.print(d_yaw);
-//  Serial.print('\t');
-
-  // Calculate the new pose (x, y, and theta)
-  float d_x = cos(d_yaw) * average_distance;
-  float d_y = sin(d_yaw) * average_distance;
-
-//  Serial.print(d_x);
-//  Serial.print('\t');
-//  Serial.println(d_y);
-
-  float cos_yaw = cos(float(telem_msg.odom_pos_yaw));
-  float sin_yaw = sin(float(telem_msg.odom_pos_yaw));
-
-  telem_msg.odom_pos_x += cos_yaw*d_x - sin_yaw*d_y;
-  telem_msg.odom_pos_y += sin_yaw*d_x + cos_yaw*d_y;
-  telem_msg.odom_pos_yaw += d_yaw;
-
-//  if (telem_msg.odom_pos_yaw > PI)
-//    telem_msg.odom_pos_yaw -= TWO_PI;
-//  else if (telem_msg.odom_pos_yaw < -PI)
-//    telem_msg.odom_pos_yaw += TWO_PI;
 
   float d_time = 1e-6 * (float)step_time_us;
   telem_msg.odom_vel_x = average_distance / d_time;
