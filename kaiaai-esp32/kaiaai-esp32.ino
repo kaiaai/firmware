0b729a3d4e90a9bb8b99fa22d66067e8c0435b10
Ilia O. <Ilia O.>
2024-09-06 22:25:57 -0700

    Show custom in web config

--- kaiaai-esp32/kaiaai-esp32.ino
+++ kaiaai-esp32/kaiaai-esp32.ino
@@ -350,11 +350,11 @@ void calcOdometry(unsigned long step_time_us, float joint_pos_delta_right,
   // TODO use Runge-Kutta integration for better accuracy
   float average_distance = (distance_right + distance_left) * 0.5;
   float d_yaw = (distance_left - distance_right)*cfg.wheel_track_recip;
-  if (abs(d_yaw) > 1) {
-    printNB("WARNING: odometry asin() domain out of bounds. Check motor encoders.");
-    d_yaw = d_yaw > 0 ? 1 : -1;
-  }
-  d_yaw = asin(d_yaw);
+//  if (abs(d_yaw) > 1) {
+//    printNB("WARNING: odometry asin() domain out of bounds. Check motor encoders.");
+//    d_yaw = d_yaw > 0 ? 1 : -1;
+//  }
+//  d_yaw = asin(d_yaw);
 
   // Average angle during the motion
   float average_angle = d_yaw*0.5 + telem_msg.odom_pos_yaw;
